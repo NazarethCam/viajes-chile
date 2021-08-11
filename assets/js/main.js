5d551eb6d0075fd96166b1c5923cde92127d3414
Nazareth Camargo <naza3192@gmail.com>
2021-08-11 17:19:10 -0400

    ordenamiento de documento

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
         });
     });
 });
-
+// Smooth Scroll.
 $("a").click(function (event) {
     if (this.hash !== "")
         event.preventDefault();
@@ -19,13 +19,13 @@ $("a").click(function (event) {
         window.location.hash = gato;
     });
 });
-
+// Movimiento Hover.
 $( "h3.movi" ).hover(function() {
 	$( this ).fadeOut( 100 );
 	$( this ).fadeIn( 500 );
 
   });
-
+// Movimiento Navbar.
   $(window).scroll(function(){
     var scroll = jQuery(window).scrollTop();
     if (scroll >= 100) {
@@ -40,7 +40,7 @@ $( "h3.movi" ).hover(function() {
   $( ".mov-1" ).on( "mouseover", function() {
 	$( this ).css( "color", "orange" );
   });
-
+// Cambio color icono al hacer doble click.
   $('.fas').dblclick(function() {
     var theColorIs = $(this).css("background-color");
     theColorIs.split(" ").join('')
