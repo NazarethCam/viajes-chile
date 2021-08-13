02f701c01947d67be6d0150bc451ca57a44675bb
Nazareth Camargo <naza3192@gmail.com>
2021-08-12 22:01:52 -0400

    modificaciones nabvar tooltip footer modal y carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,18 @@
 $(document).ready(function () {
-    
+  
     $(".card-title").click(function () {
         $(".card-text").toggle("slow", function () {
             // Animation complete.
         });
     });
 });
-// Smooth Scroll.
-$("a").click(function (event) {
-    if (this.hash !== "")
-        event.preventDefault();
 
-    var gato = this.hash;
-
-    $("html, body").animate({
-        scrolltop: $(gato).offset().top
-    }, 800, function () {
-        window.location.hash = gato;
-    });
-});
 // Movimiento Hover.
 $( "h3.movi" ).hover(function() {
 	$( this ).fadeOut( 100 );
 	$( this ).fadeIn( 500 );
 
+  
   });
 // Movimiento Navbar.
   $(window).scroll(function(){
@@ -52,4 +41,20 @@ $( "h3.movi" ).hover(function() {
         $(this).css('background-color', '#19A3B8')
     }
 });
-    
+// Tooltip
+$('[data-toggle="tooltip"]').tooltip();  
+
+// Smooth Scroll.
+	$("a").click(function(event) {
+	if (this.hash !== "") {
+	event.preventDefault();
+	
+	var gato = this.hash;
+	
+	$('html, body').animate({
+	scrollTop: $(gato).offset().top
+	}, 800, function(){
+window.location.hash = gato;
+});
+} 
+})
